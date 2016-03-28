d71b41ec668e68ab78931b7d4cd271fe40220eb9
Manav Bharambe <manavbharambe93@gmail.com>
2016-03-28 22:54:41 +0530

    trying to make admin/ in heeroku

--- product_search/urls.py
+++ product_search/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # url(r'^$', 'product_search.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^search/', include('search.urls')),
 ]
