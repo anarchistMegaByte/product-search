c8f4aa14064080e60822ec9e4c183d48d352c05d
Manav Bharambe <manavbharambe93@gmail.com>
2016-04-01 08:44:27 +0530

    type error issue now finally resolved after checking

--- search/views.py
+++ search/views.py
@@ -7,8 +7,10 @@ from django.core import serializers
 def search_flipkart(request):
 	equest = urllib.request.Request("https://affiliate-api.flipkart.net/affiliate/search/json?query=sony+mobiles&resultCount=5")
 	equest.add_header('Fk-Affiliate-Id', 'manavbhar')
-	equest.add_header('Fk-Affiliate-Token','7dfa8bde88654fd1952affc8s43a76cc5')
+	equest.add_header('Fk-Affiliate-Token','55b95905e3734978bc1aa18df555248a')
 	contents = urllib.request.urlopen(equest).read().decode("utf-8")
 	html = '<h2>I m here re just checking you out</h2>'
 	#return HttpResponse(html)
 	return JsonResponse(contents,safe=False)
+
+	
