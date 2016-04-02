d11fa6db754b968880ee74496ce2612ee1a9b0bc
Manav Bharambe <manavbharambe93@gmail.com>
2016-04-02 23:01:28 +0530

    finally done with search/get_text url

--- search/urls.py
+++ search/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-   #url(r'^', views.search_flipkart, name='search_flipkart'),
+   #	url(r'^', views.search_flipkart, name='search_flipkart'),
    url(r'^get_text/', views.get_text, name='get_text')
 ]
