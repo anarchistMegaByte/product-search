6db264afe3ab3472c25e3ce796f37f86ab1d3cd5
Manav Bharambe <manavbharambe93@gmail.com>
2016-04-02 15:56:35 +0530

    checking first get_text link

--- search/views.py
+++ search/views.py
@@ -1,3 +1,4 @@
+import json
 import urllib.request
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
@@ -5,12 +6,20 @@ from django.core import serializers
 # Create your views here.
 
 def search_flipkart(request):
-	equest = urllib.request.Request("https://affiliate-api.flipkart.net/affiliate/search/json?query=sony+mobiles&resultCount=5")
+	equest = urllib.request.Request("https://affiliate-api.flipkart.net/affiliate/search/json?query=adidas+golf+shoes&resultCount=5")
 	equest.add_header('Fk-Affiliate-Id', 'manavbhar')
 	equest.add_header('Fk-Affiliate-Token','55b95905e3734978bc1aa18df555248a')
 	contents = urllib.request.urlopen(equest).read().decode("utf-8")
-	html = '<h2>I m here re just checking you out</h2>'
 	#return HttpResponse(html)
 	return JsonResponse(contents,safe=False)
+
+def get_text(request):
+	if request.method == 'POST':
+		json_object = request.body
+		print(body)
+	else:
+		print("cool")
+		contents1 = {"Name":"BhenChod I m In"}
+	return JsonResponse(contents1,safe=False)	
 
 	
